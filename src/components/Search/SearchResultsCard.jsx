aefc9f618c0c4bfa6b6def5452c5da5b3b8f398f
sirdan <stanford303@gmail.com>
2022-10-03 11:53:12 -0700

    added css and finished up

--- src/components/Search/SearchResultsCard.jsx
+++ src/components/Search/SearchResultsCard.jsx
@@ -1,8 +1,11 @@
 // import React from 'react';
 import styles from './Search.css';
 
+
 export default function SearchResultCard({ pokemon, infinScrollRef }) {
+  console.log(pokemon);
   return <li className = {styles.SearchResultCard} ref = {infinScrollRef}>
     {pokemon.pokemon}
+    <img src={pokemon.url_image}/>
   </li>;
 }
